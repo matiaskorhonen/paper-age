e92531ce95e0b5471ac3b145a83cc4e164933ced
Matias Korhonen <me@matiaskorhonen.fi>
2023-02-12 18:42:20 +0200

    Fixes the manpage generation

--- build.rs
+++ build.rs
@@ -1,18 +1,21 @@
-// use clap::CommandFactory;
+use clap::CommandFactory;
 
-// #[path = "src/cli.rs"]
-// mod cli;
+#[path = "src/cli.rs"]
+mod cli;
+
+#[path = "src/page.rs"]
+mod page;
 
 fn main() -> std::io::Result<()> {
-    // let out_dir =
-    //     std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?);
-    // let cmd = cli::Args::command();
+    let out_dir =
+        std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?);
+    let cmd = cli::Args::command();
 
-    // let man = clap_mangen::Man::new(cmd);
-    // let mut buffer: Vec<u8> = Default::default();
-    // man.render(&mut buffer)?;
+    let man = clap_mangen::Man::new(cmd);
+    let mut buffer: Vec<u8> = Default::default();
+    man.render(&mut buffer)?;
 
-    // std::fs::write(out_dir.join("paper-age.1"), buffer)?;
+    std::fs::write(out_dir.join("paper-age.1"), buffer)?;
 
     Ok(())
 }
