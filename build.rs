e249574d775f83de403c36c216f3bf6147a5b868
Matias Korhonen <me@matiaskorhonen.fi>
2023-02-12 21:36:24 +0200

    Create shell completion files

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 use clap::CommandFactory;
+use clap_complete::{generate_to, shells::Shell};
 
 #[path = "src/cli.rs"]
 mod cli;
@@ -9,14 +10,19 @@ pub mod page;
 fn main() -> std::io::Result<()> {
     let out_dir =
         std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?);
-    let cmd = cli::Args::command();
+    let mut cmd = cli::Args::command();
 
-    let man = clap_mangen::Man::new(cmd);
+    let man = clap_mangen::Man::new(cmd.clone());
     let mut buffer: Vec<u8> = Default::default();
     man.render(&mut buffer)?;
 
     // Write to the same level as the binary, even though build scripts shouldn't
     std::fs::write(out_dir.join("../../../paper-age.1"), buffer)?;
+
+    for shell in [Shell::Bash, Shell::Fish, Shell::Zsh] {
+        let path = generate_to(shell, &mut cmd, "paper-age", out_dir.join("../../../"))?;
+        println!("cargo:info=completion file is generated: {:?}", path);
+    }
 
     Ok(())
 }
