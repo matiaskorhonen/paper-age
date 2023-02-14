05870673e706b40b977fc9dc0a9cb63b98201cc7
Matias Korhonen <me@matiaskorhonen.fi>
2023-02-14 16:12:24 +0200

    Add man page and shell completion to the release (#20)
    * Fixes the manpage generation
    
    * Update build.rs
    
    * Include the man page with the release
    
    * Create shell completion files
    
    * Include the completion files with the archive
    
    * Put the man page & completion files in directories
    
    * Fix the paths
    
    * Fix Clippy warnings
    
    * Remove warnings from build file
    
    * Remove the redundant clone

--- build.rs
+++ build.rs
@@ -1,18 +1,46 @@
-// use clap::CommandFactory;
+use clap::CommandFactory;
+use clap_complete::{generate_to, shells::Shell};
+use path_absolutize::*;
 
-// #[path = "src/cli.rs"]
-// mod cli;
+#[path = "src/cli.rs"]
+mod cli;
+
+#[path = "src/page.rs"]
+pub mod page;
 
 fn main() -> std::io::Result<()> {
-    // let out_dir =
-    //     std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?);
-    // let cmd = cli::Args::command();
+    let out_dir =
+        std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?);
+    let mut cmd = cli::Args::command();
 
-    // let man = clap_mangen::Man::new(cmd);
-    // let mut buffer: Vec<u8> = Default::default();
-    // man.render(&mut buffer)?;
+    let man = clap_mangen::Man::new(cmd.clone());
+    let mut buffer: Vec<u8> = Default::default();
+    man.render(&mut buffer)?;
 
-    // std::fs::write(out_dir.join("paper-age.1"), buffer)?;
+    // Create a man directory at the same level as the binary, even though build
+    // scripts shouldn't
+    let man_dir = &out_dir.join("../../../man");
+    let absolute_man_dir = man_dir.absolutize()?;
+    std::fs::create_dir_all(&absolute_man_dir)?;
+    let man_path = absolute_man_dir.join("paper-age.1");
+    std::fs::write(man_path, buffer)?;
+
+    // Create a completion directory the same level as the binary
+    let completion_dir = out_dir.join("../../../completion");
+    let absolute_completion_dir = completion_dir.absolutize()?;
+    std::fs::create_dir_all(absolute_completion_dir.clone())?;
+    for shell in [Shell::Bash, Shell::Fish, Shell::Zsh] {
+        generate_to(
+            shell,
+            &mut cmd,
+            "paper-age",
+            absolute_completion_dir.as_ref(),
+        )?;
+    }
+
+    // Re-run if the cli or page files change
+    println!("cargo:rerun-if-changed=src/cli.rs");
+    println!("cargo:rerun-if-changed=src/page.rs");
 
     Ok(())
 }
